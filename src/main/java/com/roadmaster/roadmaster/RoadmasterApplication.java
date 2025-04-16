85ecc0b68051def88dd5016f82de2de8f267687c
Abbas-514 <ma6063581@gmail.com>
2025-04-17 00:40:34 +0500

    Added Spring Boot backend for RoadMaster

---
+++ src/main/java/com/roadmaster/roadmaster/RoadmasterApplication.java
@@ -0,0 +1,13 @@
+package com.roadmaster.roadmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadmasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoadmasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roadmaster/roadmaster/RoadmasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roadmaster.roadmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadmasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
